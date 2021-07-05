287529be035fd72d98ef5726ab8c85ca2bcd618c
Lucas Elliott <lucas_github@itcommand.net>
2021-07-05 19:59:39 -0400

    first update

---
+++ LFC.bat
@@ -0,0 +1,33 @@
+@echo off
+title Lukes Friend Checker - Offline
+setlocal enabledelayedexpansion
+@mode con lines=16 cols=50
+:menu
+cls
+
+cd bincd
+echo [102;30m              Luke's Friend Checker               [0m
+if exist ..\Data\usr.ini (
+	set /p usr=<..\Data\usr.ini
+	set usr=%usr: =%
+	set bincd=%cd%
+) Else (
+	set usr=User
+)
+echo.
+echo This tool is designed to tell you when your
+echo friends are available to hang out digitally.
+echo.
+echo [4mWhen you are available[0m go online and you will see
+echo if any other users are online too you'll see them.
+echo If a user goes online or offline while you are
+echo online, you'll get a notification.
+echo.
+echo To avoid distractions, you cannot see if other
+echo users are online unless you are. For the best
+echo results [4malways[0m use this tool and be honest.
+echo.
+echo Go online now? (Press Y).
+choice /n >nul 2>nul
+if %errorlevel%==1 "%bincd%..\Friend Checker.bat"
+goto menu
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Lukes-Friend-Checker
- 
+
+This was just for me and a friend.
+
+It's designed to easily tell if your friends are online and available to hang out.
+
+
+
+The home directory for the ftp user **must** have two folders: `Users` and `GamerStatus`. Within the `GamerStatus` Folder there must be one folder, `Online`. Ideally I'd write the code to set that up for you but,
+
+![](https://media4.giphy.com/media/yBnI9iojWdID6/giphy.gif?cid=ecf05e47ycving19l1rv0bkrl1e72alsw7sxt8id8hiilij7&rid=giphy.gif&ct=g)
+
+It also stores FTP server logins in plain text, so if you do use this, use it with an isolated user and bandwidth limits. Ideally I'd write it in another language or at least use PowerShell to encrypt the logins for you but, once again,
+
+![](https://media4.giphy.com/media/yBnI9iojWdID6/giphy.gif?cid=ecf05e47ycving19l1rv0bkrl1e72alsw7sxt8id8hiilij7&rid=giphy.gif&ct=g)
+
+So yeah.
